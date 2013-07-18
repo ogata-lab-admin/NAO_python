81913d6e0f26f2d365831e36788085b7bffed93f
ysuga <ysuga@ysuga.net>
2013-07-18 13:06:54 +0900

    Update NAO.idl

--- NAO.py
+++ NAO.py
@@ -36,7 +36,7 @@ nao_spec = ["implementation_id", "NAO",
 		 "type_name",         "NAO", 
 		 "description",       "NAO RT Component", 
 		 "version",           "1.0.0", 
-		 "vendor",            "Sugar Sweet Robotics", 
+		 "vendor",            "Ogata Lab Waseda Univ.", 
 		 "category",          "Robot", 
 		 "activity_type",     "STATIC", 
 		 "max_instance",      "1", 
@@ -45,9 +45,29 @@ nao_spec = ["implementation_id", "NAO",
 		 "conf.default.ipaddress", "nao.local",
 		 "conf.default.port", "9559",
 		 "conf.default.debug", "1",
+		 "conf.default.camera_id", "0",
+		 "conf.default.num_channel", "1",
+		 "conf.default.interleaved", "1",
+		 "conf.default.num_samples", "16000",
+		 "conf.default.channel_name", "fromt",
+		 "conf.default.enable_camera", "0",
+		 "conf.default.enable_audio", "0",
 		 "conf.__widget__.ipaddress", "text",
 		 "conf.__widget__.port", "text",
 		 "conf.__widget__.debug", "text",
+		 "conf.__widget__.camera_id", "radio",
+		 "conf.__widget__.num_channel", "radio",
+		 "conf.__widget__.interleaved", "radio",
+		 "conf.__widget__.num_samples", "text",
+		 "conf.__widget__.channel_name", "checkbox",
+		 "conf.__widget__.enable_camera", "radio",
+		 "conf.__widget__.enable_audio", "radio",
+		 "conf.__constraints__.camera_id", "(0, 1)",
+		 "conf.__constraints__.num_channel", "(1,2,4)",
+		 "conf.__constraints__.interleaved", "(0, 1)",
+		 "conf.__constraints__.channel_name", "(front, rear, left right)",
+		 "conf.__constraints__.enable_camera", "(0, 1)",
+		 "conf.__constraints__.enable_audio", "(0,1)",
 		 ""]
 # </rtc-template>
 
@@ -65,13 +85,44 @@ class NAO(OpenRTM_aist.DataFlowComponentBase):
 	def __init__(self, manager):
 		OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
 
+		w = 640
+		h = 480
+		bpp = 24
+		format = "bitmap"
+		fDiv = 1.0
+		pixels = []
+		self._d_camera = RTC.CameraImage(RTC.Time(0,0), w, h, bpp, format, fDiv, pixels)
+		"""
+		"""
+		self._cameraOut = OpenRTM_aist.OutPort("camera", self._d_camera)
+		self._d_audio = RTC.TimedOctetSeq(RTC.Time(0,0),[])
+		"""
+		"""
+		self._audioOut = OpenRTM_aist.OutPort("audio", self._d_audio)
+		self._d_jointAngle = RTC.TimedDoubleSeq(RTC.Time(0,0),[])
+		"""
+		"""
+		self._jointAngleOut = OpenRTM_aist.OutPort("jointAngle", self._d_jointAngle)
+		self._d_bumper = RTC.TimedBooleanSeq(RTC.Time(0,0),[])
+		"""
+		bumper data (lfoot_left, lfoot_right, rfoot_left, rfoot_right)
+		"""
+		self._bumperOut = OpenRTM_aist.OutPort("bumper", self._d_bumper)
+		self._d_touch = RTC.TimedBooleanSeq(RTC.Time(0,0),[])
+		"""
+		touch detection (head_back, head_center, head_front, rhand_back,
+		rhand_center, rhand_front, lhand_back, lhand_center, lhand_front)
+		"""
+		self._touchOut = OpenRTM_aist.OutPort("touch", self._d_touch)
+		self._d_sonar = RTC.TimedBooleanSeq(RTC.Time(0,0),[])
+		"""
+		sonar detection, (left, right)
+		"""
+		self._sonarOut = OpenRTM_aist.OutPort("sonar", self._d_sonar)
 
 		"""
 		"""
-		self._ALMotionPort = OpenRTM_aist.CorbaPort("ALMotion")
-		"""
-		"""
-		self._ALTextToSpeechPort = OpenRTM_aist.CorbaPort("ALTextToSpeech")
+		self._NAO_srvPort = OpenRTM_aist.CorbaPort("NAO_srv")
 
 		"""
 		"""
@@ -79,6 +130,18 @@ class NAO(OpenRTM_aist.DataFlowComponentBase):
 		"""
 		"""
 		self._textToSpeech = ALTextToSpeech_i()
+		"""
+		"""
+		self._behaviorManager = ALBehaviorManager_i()
+		"""
+		"""
+		self._memory = ALMemory_i()
+		"""
+		"""
+		self._leds = ALLeds_i()
+		"""
+		"""
+		self._videoDevice = ALVideoDevice_i()
 		
 
 
@@ -102,6 +165,48 @@ class NAO(OpenRTM_aist.DataFlowComponentBase):
 		 - DefaultValue: 1
 		"""
 		self._debug = [1]
+		"""
+		
+		 - Name:  camera_id
+		 - DefaultValue: 0
+		"""
+		self._camera_id = [0]
+		"""
+		
+		 - Name:  num_channel
+		 - DefaultValue: 1
+		"""
+		self._num_channel = [1]
+		"""
+		
+		 - Name:  interleaved
+		 - DefaultValue: 1
+		"""
+		self._interleaved = [1]
+		"""
+		
+		 - Name:  num_samples
+		 - DefaultValue: 16000
+		"""
+		self._num_samples = [16000]
+		"""
+		
+		 - Name:  channe_name
+		 - DefaultValue: fromt
+		"""
+		self._channe_name = ['fromt']
+		"""
+		
+		 - Name:  enable_camera
+		 - DefaultValue: 0
+		"""
+		self._enable_camera = [0]
+		"""
+		
+		 - Name:  enable_audio
+		 - DefaultValue: 0
+		"""
+		self._enable_audio = [0]
 		
 		# </rtc-template>
 
@@ -120,20 +225,36 @@ class NAO(OpenRTM_aist.DataFlowComponentBase):
 		self.bindParameter("ipaddress", self._ipaddress, "nao.local")
 		self.bindParameter("port", self._port, "9559")
 		self.bindParameter("debug", self._debug, "1")
+		self.bindParameter("camera_id", self._camera_id, "0")
+		self.bindParameter("num_channel", self._num_channel, "1")
+		self.bindParameter("interleaved", self._interleaved, "1")
+		self.bindParameter("num_samples", self._num_samples, "16000")
+		self.bindParameter("channel_name", self._channe_name, "fromt")
+		self.bindParameter("enable_camera", self._enable_camera, "0")
+		self.bindParameter("enable_audio", self._enable_audio, "0")
 		
 		# Set InPort buffers
 		
 		# Set OutPort buffers
+		self.addOutPort("camera",self._cameraOut)
+		self.addOutPort("audio",self._audioOut)
+		self.addOutPort("jointAngle",self._jointAngleOut)
+		self.addOutPort("bumper",self._bumperOut)
+		self.addOutPort("touch",self._touchOut)
+		self.addOutPort("sonar",self._sonarOut)
 		
 		# Set service provider to Ports
-		self._ALMotionPort.registerProvider("ALMotion", "ssr::ALMotion", self._motion)
-		self._ALTextToSpeechPort.registerProvider("ALTextToSpeech", "ssr::ALTextToSpeech", self._textToSpeech)
+		self._NAO_srvPort.registerProvider("ALMotion", "ssr::ALMotion", self._motion)
+		self._NAO_srvPort.registerProvider("ALTextToSpeech", "ssr::ALTextToSpeech", self._textToSpeech)
+		self._NAO_srvPort.registerProvider("ALBehaviorManager", "ssr::ALBehaviorManager", self._behaviorManager)
+		self._NAO_srvPort.registerProvider("ALMemory", "ssr::ALMemory", self._memory)
+		self._NAO_srvPort.registerProvider("ALLeds", "ssr::ALLeds", self._leds)
+		self._NAO_srvPort.registerProvider("ALVideoDevice", "ssr::ALVideoDevice", self._videoDevice)
 		
 		# Set service consumers to Ports
 		
 		# Set CORBA Service Ports
-		self.addPort(self._ALMotionPort)
-		self.addPort(self._ALTextToSpeechPort)
+		self.addPort(self._NAO_srvPort)
 		
 		return RTC.RTC_OK
 	
@@ -188,7 +309,11 @@ class NAO(OpenRTM_aist.DataFlowComponentBase):
 		#
 		#
 	def onActivated(self, ec_id):
-	
+		try :
+			self._textToSpeech.connect(self._ipaddress[0], self._port[0])
+			self._behaviorManager.connect(self._ipaddress[0], self._port[0])
+		except Exception, e:
+			print e
 		return RTC.RTC_OK
 	
 		##
@@ -216,7 +341,7 @@ class NAO(OpenRTM_aist.DataFlowComponentBase):
 		#
 		#
 	def onExecute(self, ec_id):
-	
+		
 		return RTC.RTC_OK
 	
 	#	##
--- NAO_idl_example.py
+++ NAO_idl_example.py
@@ -12,7 +12,8 @@
 
 import omniORB
 from omniORB import CORBA, PortableServer
-import ssr, ssr__POAimport ssr, ssr__POA
+import ssr, ssr__POA
+from naoqi import *
 
 
 class ALMotion_i (ssr__POA.ALMotion):
@@ -28,13 +29,13 @@ class ALMotion_i (ssr__POA.ALMotion):
         """
         pass
 
-    # void setAngles(in StringArray name, in DoubleArray value, in double fractionSpeed)
+    # void setAngles(in StringArray name, in FloatArray value, in float fractionSpeed)
     def setAngles(self, name, value, fractionSpeed):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: None
 
-    # DoubleArray getAngles(in StringArray name, in bool useSensors)
+    # FloatArray getAngles(in StringArray name, in boolean useSensors)
     def getAngles(self, name, useSensors):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
@@ -52,13 +53,13 @@ class ALMotion_i (ssr__POA.ALMotion):
         # *** Implement me
         # Must return: None
 
-    # void setStiffness(in StringArray name, in DoubleArray stiffnesses)
+    # void setStiffness(in StringArray name, in FloatArray stiffnesses)
     def setStiffness(self, name, stiffnesses):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: None
 
-    # DoubleArray getStiffness(in StringARray name)
+    # FloatArray getStiffness(in StringArray name)
     def getStiffness(self, name):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
@@ -76,25 +77,25 @@ class ALMotion_i (ssr__POA.ALMotion):
         # *** Implement me
         # Must return: None
 
-    # void moveTo(in double x, in double y, in double theta)
+    # void moveTo(in float x, in float y, in float theta)
     def moveTo(self, x, y, theta):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: None
 
-    # void moveToward(in double vx, in double vy, in double vtheta)
+    # void moveToward(in float vx, in float vy, in float vtheta)
     def moveToward(self, vx, vy, vtheta):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: None
 
-    # DoubleArray getRobotPosition()
+    # FloatArray getRobotPosition()
     def getRobotPosition(self):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: result
 
-    # DoubleArray getRobotVelocity()
+    # FloatArray getRobotVelocity()
     def getRobotVelocity(self):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
@@ -106,7 +107,7 @@ class ALMotion_i (ssr__POA.ALMotion):
         # *** Implement me
         # Must return: None
 
-    # bool moveIsActive()
+    # boolean moveIsActive()
     def moveIsActive(self):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
@@ -124,7 +125,7 @@ class ALMotion_i (ssr__POA.ALMotion):
         # *** Implement me
         # Must return: None
 
-    # void setWalkArmEnabled(in bool leftArmEnable, in bool rightArmEnable)
+    # void setWalkArmEnabled(in boolean leftArmEnable, in boolean rightArmEnable)
     def setWalkArmEnabled(self, leftArmEnable, rightArmEnable):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
@@ -136,25 +137,25 @@ class ALMotion_i (ssr__POA.ALMotion):
         # *** Implement me
         # Must return: result
 
-    # DoubleArray getPosition(in string name, in int space, in bool useSensors)
+    # FloatArray getPosition(in string name, in long space, in boolean useSensors)
     def getPosition(self, name, space, useSensors):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: result
 
-    # void setPosition(in string name, in int space, in DoubleArray position, in double fractionMaxSpeed, in int axisMask)
+    # void setPosition(in string name, in long space, in FloatArray position, in float fractionMaxSpeed, in long axisMask)
     def setPosition(self, name, space, position, fractionMaxSpeed, axisMask):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: None
 
-    # void setTransform(in string name, in int space, in DoubleArray transform, in double fractionMaxSpeed, in int axisMask)
+    # void setTransform(in string name, in long space, in FloatArray transform, in float fractionMaxSpeed, in long axisMask)
     def setTransform(self, name, space, transform, fractionMaxSpeed, axisMask):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: None
 
-    # DoubleArray getTransform(in string name, in int space, in bool useSensorValues)
+    # FloatArray getTransform(in string name, in long space, in boolean useSensorValues)
     def getTransform(self, name, space, useSensorValues):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
@@ -174,42 +175,288 @@ class ALTextToSpeech_i (ssr__POA.ALTextToSpeech):
         Initialise member variables here
         """
         pass
+    def connect(self, addr, port):
+        self.proxy = ALProxy("ALTextToSpeech", addr, port)
 
     # void say(in string stringToSay)
     def say(self, stringToSay):
-        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: None
+        self.proxy.say(stringToSay)
 
-    # void setVolume(in double volume)
+    # void setVolume(in float volume)
     def setVolume(self, volume):
-        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: None
+        self.proxy.setVolume(volume)
 
-    # double getVolume()
+    # float getVolume()
     def getVolume(self):
-        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: result
+        return self.proxy.getVolume()
 
     # void setLanguage(in string language)
     def setLanguage(self, language):
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+        self.proxy.setLanguage(language)
+
+    # string getLanguage()
+    def getLanguage(self):
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+        return self.proxy.getLanguage()
+
+    # StringArray getAvailableLanguages()
+    def getAvailableLanguages(self):
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+        return self.proxy.getAvailableLanguages()
+
+
+class ALBehaviorManager_i (ssr__POA.ALBehaviorManager):
+    """
+    @class ALBehaviorManager_i
+    Example class implementing IDL interface ssr.ALBehaviorManager
+    """
+
+    def __init__(self):
+        """
+        @brief standard constructor
+        Initialise member variables here
+        """
+        pass
+
+    def connect(self, addr, port):
+        self.proxy = ALProxy("ALBehaviorManager", addr, port)
+        pass
+
+    # StringArray getInstalledBehaviors()
+    def getInstalledBehaviors(self):
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+        return self.proxy.getInstalledBehaviors()
+
+    # StringArray getRunningBehaviors()
+    def getRunningBehaviors(self):
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+        return self.proxy.getRunningBehaviors()
+
+    # boolean isBehaviorInstalled(in string name)
+    def isBehaviorInstalled(self, name):
+        #raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+        return self.proxy.isBehaviorInstalled(name)
+
+    # boolean isBehaviorRunning(in string name)
+    def isBehaviorRunning(self, name):
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+        return self.proxy.isBehaviorRunning(name)
+
+    # void runBehavior(in string name)
+    def runBehavior(self, name):
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+        self.proxy.runBehavior(name)
+
+    # void stopAllBehaviors()
+    def stopAllBehaviors(self):
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+        self.proxy.stopAllBehaviors()
+
+    # void stopBehavior(in string name)
+    def stopBehavior(self, name):
+        # raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+        self.proxy.stopBehavior(name)
+
+
+
+class ALMemory_i (ssr__POA.ALMemory):
+    """
+    @class ALMemory_i
+    Example class implementing IDL interface ssr.ALMemory
+    """
+
+    def __init__(self):
+        """
+        @brief standard constructor
+        Initialise member variables here
+        """
+        pass
+
+    # void insertLongData(in string name, in long value)
+    def insertLongData(self, name, value):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: None
 
-    # string getLanguage()
-    def getLanguage(self):
+    # void insertFloatData(in string name, in long value)
+    def insertFloatData(self, name, value):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+
+    # void insertStringData(in string name, in string value)
+    def insertStringData(self, name, value):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+
+    # void insertoLongArrayData(in string name, in LongArray value)
+    def insertoLongArrayData(self, name, value):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+
+    # void insertoFloatArrayData(in string name, in FloatArray value)
+    def insertoFloatArrayData(self, name, value):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+
+    # void insertoStringArrayData(in string name, in StringArray value)
+    def insertoStringArrayData(self, name, value):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+
+    # long getLongData(in string name)
+    def getLongData(self, name):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: result
 
-    # StringArray getAvailableLanguages()
-    def getAvailableLanguages(self):
+    # float getFloatData(in string name)
+    def getFloatData(self, name):
         raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
         # *** Implement me
         # Must return: result
+
+    # string getStringData(in string name)
+    def getStringData(self, name):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+    # LongArray getLongArrayData(in string name)
+    def getLongArrayData(self, name):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+    # FloatArray getFloatArrayData(in string name)
+    def getFloatArrayData(self, name):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+    # StringArray getStringArrayData(in string name)
+    def getStringArrayData(self, name):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+
+
+class ALVideoDevice_i (ssr__POA.ALVideoDevice):
+    """
+    @class ALVideoDevice_i
+    Example class implementing IDL interface ssr.ALVideoDevice
+    """
+
+    def __init__(self):
+        """
+        @brief standard constructor
+        Initialise member variables here
+        """
+        pass
+
+    # long getCameraModel(in long index)
+    def getCameraModel(self, index):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+    # long getFrameRate(in long index)
+    def getFrameRate(self, index):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+    # long getResolution(in long index)
+    def getResolution(self, index):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+    # long getColorSpace(in long index)
+    def getColorSpace(self, index):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+    # boolean setCameraParameter(in long id, in long value)
+    def setCameraParameter(self, id, value):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+    # long getCameraParameter(in long id)
+    def getCameraParameter(self, id):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+    # boolean setCameraParameterToDefault(in long id)
+    def setCameraParameterToDefault(self, id):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: result
+
+
+
+class ALLeds_i (ssr__POA.ALLeds):
+    """
+    @class ALLeds_i
+    Example class implementing IDL interface ssr.ALLeds
+    """
+
+    def __init__(self):
+        """
+        @brief standard constructor
+        Initialise member variables here
+        """
+        pass
+
+    # void fade(in string name, in float intensity, in float duration)
+    def fade(self, name, intensity, duration):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
+
+    # void fadeRGB(in string name, in long rgb, in float duration)
+    def fadeRGB(self, name, rgb, duration):
+        raise CORBA.NO_IMPLEMENT(0, CORBA.COMPLETED_NO)
+        # *** Implement me
+        # Must return: None
 
 
 if __name__ == "__main__":
